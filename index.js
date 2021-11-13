02a77e0fd1c3d42a8c697772343cb2c7f567dfb8
fardin-hasan <19-40043-1@student.aiub.edu>
2021-11-13 13:56:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+require('dotenv').config()
+var cors = require('cors')
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ex382.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("products");
+        const allProducts = database.collection("productsCollections");
+        const PurchaseCollection = database.collection("PurchaseCollection");
+        const customerReview = database.collection("customerReview");
+        console.log('done')
+
+        // find limited data for homepage
+        app.get('/products', async (req, res) => {
+            const cursor = allProducts.find({});
+            const products = await cursor.limit(6).toArray();
+            res.send(products);
+        });
+
+        // find all products
+        app.get('/allProducts', async (req, res) => {
+            const cursor = allProducts.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // single product based on product id 
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await allProducts.findOne(query);
+            res.send(user);
+        });
+
+        // post purchase
+        app.post('/purchase', async (req, res) => {
+
+            const purchase = req.body;
+            const result = await PurchaseCollection.insertOne(purchase);
+            res.json(result);
+
+        });
+
+        // customer Review post
+        app.post('/customerReview', async (req, res) => {
+
+            const review = req.body;
+            const result = await customerReview.insertOne(review);
+            res.json(result);
+
+        });
+        // show customer reviews
+        app.get('/customerReview', async (req, res) => {
+            const cursor = customerReview.find({});
+            const review = await cursor.toArray();
+            res.send(review);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
